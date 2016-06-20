81ee4f85b90eaf47e3a272de0a3aa0d283aab82d
Pea Lutz <pea@glocal.coop>
2016-06-20 00:59:41 -0400

    Updated Woocommerce

--- plugins/woocommerce/templates/single-product/add-to-cart/variation.php
+++ plugins/woocommerce/templates/single-product/add-to-cart/variation.php
@@ -5,7 +5,7 @@
  * This is a javascript-based template for single variations (see https://codex.wordpress.org/Javascript_Reference/wp.template).
  * The values will be dynamically replaced after selecting attributes.
  *
- * @see 	http://docs.woothemes.com/document/template-structure/
+ * @see 	https://docs.woothemes.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 2.5.0
@@ -23,7 +23,9 @@ if ( ! defined( 'ABSPATH' ) ) {
         {{{ data.variation.price_html }}}
     </div>
 
-    
+    <div class="woocommerce-variation-availability">
+        {{{ data.variation.availability_html }}}
+    </div>
 </script>
 <script type="text/template" id="tmpl-unavailable-variation-template">
     <p><?php _e( 'Sorry, this product is unavailable. Please choose a different combination.', 'woocommerce' ); ?></p>
